556fd9c50c8238506e922703b550314a9f3f397e
Erveftick <edwardkvashin@gmail.com>
2021-05-17 16:22:45 +0300

    Created footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="container text-center">
+                <p>LEM •  ©2021 •  All rights reserved • <a href="#">Contacts</a></p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;

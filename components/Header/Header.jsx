23797ecaf8ee02de65d4128a05ce024ebd225a9c
alexey_horbunov <algoritm211@gmail.com>
2021-05-20 00:49:10 +0300

    Make adaptive header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RegistrationModal from '../Form/Registration'
 import LoginModal from '../Form/Login'
 import { Nav, Navbar } from 'react-bootstrap'
@@ -9,20 +9,36 @@ import UserImage from './UserImage'
 const Header = () => {
   const [loginModalShow, setLoginModalShow] = useState(false)
   const [registrationModalShow, setRegistrationModalShow] = useState(false)
+  const [size, setSize] = useState([0, 0])
 
   const switchModals = () => {
     setLoginModalShow((prev) => !prev)
     setRegistrationModalShow((prev) => !prev)
   }
 
+  function updateSize() {
+    setSize([document.documentElement.clientWidth, document.documentElement.clientHeight])
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
   return (
     <>
       <Navbar collapseOnSelect expand="md" bg="navbar-light">
-        <div className="container">
+        <div className="container d-flex justify-content-between">
           <Link href={'/'}>
             <Navbar.Brand href="/">LEM</Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          {size[0] <= 768 && (
+            <div className="d-flex justify-content-between">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+              <UserImage/>
+            </div>
+          )}
           <Navbar.Collapse id="responsive-navbar-nav" className={'ml-auto'}>
             <Nav className="mr-auto">
             </Nav>
@@ -45,12 +61,12 @@ const Header = () => {
                   Contacts
                 </Nav.Link>
               </Link>
-              {/*<Nav.Item href="#" className={'navigation-li px-2'}>*/}
+              {/* <Nav.Item href="#" className={'navigation-li px-2'}>*/}
               {/*  <button className="nav-link px-2 m-auto login-button navigation-li" role="button" onClick={() => setLoginModalShow(true)}>Get Started</button>*/}
-              {/*</Nav.Item>*/}
+              {/* </Nav.Item>*/}
             </Nav>
+            {size[0] > 768 && <UserImage/>}
           </Navbar.Collapse>
-          <UserImage/>
         </div>
       </Navbar>
       <RegistrationModal

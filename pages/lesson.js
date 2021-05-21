19dd71a36687127a5ecbf995bb66ccdb1aa481b1
Erveftick <edwardkvashin@gmail.com>
2021-05-22 01:50:51 +0300

    Created lesson.js

---
+++ pages/lesson.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import LessonPage from '../components/LessonPage/LessonPage'
+
+const Lesson = () => {
+  return (
+    <div>
+      <Header/>
+      <LessonPage/>
+    </div>
+  )
+}
+
+export default Lesson

7e8e345fcb41de7e91b3ab871b8ad82c5d8b1c35
Erveftick <edwardkvashin@gmail.com>
2021-05-24 23:03:19 +0300

    Created 403 and 404 errors pages

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 
-const ForbiddenPage = () => {
+const UnfoundPage = () => {
   return (
     <div className="container d-flex text-center" style={{ height: '100vh', alignItems: 'center' }}>
       <div className="row">
         <div className="col-12 col-md-4 p-5 m-auto">
-          <h2 className="category-picker my-3">Forbidden</h2>
+          <h2 className="category-picker my-3">Something went wrong!</h2>
           <Link href={'/'}>
             <a>
               <button className="content-btn">Go to main</button>
@@ -14,11 +14,11 @@ const ForbiddenPage = () => {
           </Link>
         </div>
         <div className="col-12 col-md-8 m-auto order-first order-md-last">
-          <img className="content-image" src="/403.png" alt="photo"/>
+          <img className="content-image" src="/404.png" alt="photo"/>
         </div>
       </div>
     </div>
   )
 }
 
-export default ForbiddenPage
+export default UnfoundPage

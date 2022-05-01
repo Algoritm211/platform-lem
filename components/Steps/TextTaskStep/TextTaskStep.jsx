988570b74411558d8a4ca5146ef33c153eabe2e4
ek <ek@blaster.ai>
2022-05-01 16:11:04 +0300

    If user has already answered the question, textarea will contain that answer. Otherwise - user can answer

--- components/Steps/TextTaskStep/TextTaskStep.jsx
+++ components/Steps/TextTaskStep/TextTaskStep.jsx
@@ -6,9 +6,19 @@ import { loadTextStep } from '../../../store/lessonSteps/text.thunk'
 import { useTranslation } from 'next-i18next'
 import { addStepToCompleted } from '../../../store/auth/user.thunks'
 import { getUserData } from '../../../store/auth/selectors'
-import { Button } from 'antd'
+import { Button } from 'react-bootstrap'
 import AnswerAPI from '../../../api/api.answer'
 import TextStepAPI from '../../../api/lessonTypes/api.text'
+
+const TextTaskStepTextArea = ({ value, onChange, placeholder }) => {
+  return (
+    <textarea
+      className="form-control inputAcc"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}/>
+  )
+}
 
 const TextTaskStep = ({ stepId }) => {
   const { t } = useTranslation('steps')
@@ -51,15 +61,26 @@ const TextTaskStep = ({ stepId }) => {
   return (
     <div>
       <p className="courses-lecture mt-3 mb-5" dangerouslySetInnerHTML={{ __html: currentStep.body }}/>
-      <textarea
-        className="form-control inputAcc"
-        value={answerText}
-        onChange={(event) => setAnswerText(event.target.value)}
-        placeholder={t('answer')} />
-      {/* //TODO fix layout of button */}
-      <Button disabled={!!currentStep.answer} onClick={onSubmitAnswer}>
-        Submit
-      </Button>
+      {currentStep.answer
+        ? <textarea
+          disabled
+          className="form-control inputAcc"
+          value={currentStep.answer}/>
+        : <TextTaskStepTextArea
+          value={answerText}
+          onChange={(event) => setAnswerText(event.target.value)}
+          placeholder={t('answer')}
+        />}
+
+      <div className="my-3">
+        <Button disabled={!!currentStep.answer} onClick={onSubmitAnswer}>
+          Submit
+        </Button>
+      </div>
+
+      {currentStep.answer ? <p>{t('cantChangeAnswer')}</p> : null}
+
+
     </div>
   )
 }

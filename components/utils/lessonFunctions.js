bf926262f66450fe862f210bb071ce4d3ae10a4c
Alexey Horbunov <algoritm211@gmail.com>
2022-05-20 22:41:31 +0300

    Fix percents (#11)

--- components/utils/lessonFunctions.js
+++ components/utils/lessonFunctions.js
@@ -7,26 +7,26 @@ export const countArrayEntries = (arr1, arr2) => {
 export const getStepIdArr = (array) => {
   const stepArr = array.reduce((accum, stepObj) => {
     return accum + stepObj.stepId + ','
-    // accum.push(stepObj.stepId)
   }, '')
 
   return stepArr.split(',').slice(0, -1)
 }
+
 // This function with lessons array, which you can give from courses.lessons
 export const getAllCourseStepId = (arrayLessons) => {
   let allStepsIdArr = []
   arrayLessons.forEach((lesson) => {
     const stepArr = getStepIdArr(lesson.steps)
-    // console.log(stepArr)
     allStepsIdArr = allStepsIdArr.concat(stepArr)
   })
   return allStepsIdArr
 }
 
 export const percentCompletedCourse = (userStepsCompleted, course) => {
+  const preparedUserStep = Array.from(new Set(userStepsCompleted))
   let percentCompleted = 0
-  const allSteps = getAllCourseStepId(course.lessons)
-  const completed = countArrayEntries(userStepsCompleted, allSteps)
+  const allSteps = Array.from(new Set(getAllCourseStepId(course.lessons)))
+  const completed = countArrayEntries(preparedUserStep, allSteps)
   if (completed !== 0) {
     percentCompleted = (completed * 100 / allSteps.length).toFixed(1)
   }
